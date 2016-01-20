37b79d25691eebac9f133f4fd4dc925d9d6b1e7c
Duncan Fermor <duncan@elitemail.org>
2016-01-20 09:37:55 +0000

    testing a frontend

--- spec/graph_spec.js
+++ spec/graph_spec.js
@@ -2,4 +2,27 @@ describe("a network graph", function() {
   it("has nodes", function() {
     expect(Graph().nodes).toBeDefined();
   });
+  
+  it("has links", function() {
+    expect(Graph().links).toBeDefined();
+  })
 });
+
+describe("a graph view", function() {
+  it("has a graph", function() {
+    expect(GraphView({}).graph).toBeDefined();
+  });
+  
+  it("has a render function", function() {
+    expect(GraphView({}).render).toBeDefined();
+  })
+  
+  describe(".render", function() {
+    it("produces an svg circle for each node", function() {
+      expect(GraphView({ nodes: [1, 2]}).render()).toEqual (
+        [[ "circle", { cx: 0, cy: 0} ],
+        [ "circle", { cx: 0, cy: 0} ]]
+      )
+    })
+  })
+})
--- src/graph.js
+++ src/graph.js
@@ -1,5 +1,22 @@
 function Graph() {
   return {
-    nodes: []
+    nodes: [],
+    links: []
+  }
+}
+
+function GraphView(graph) {
+  function render() {
+    var rendered_nodes = graph.nodes.map(
+      function(node) {
+        return [ "circle", { cx: 0, cy: 0 } ]
+      }
+    )
+    return rendered_nodes
+  }
+  
+  return {
+    graph: graph,
+    render: render
   }
 }
